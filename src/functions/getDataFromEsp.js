16fb9d70eda5ba213628f63129c0ee4e4d4a30a8
Huaben Chen <huabench@gmail.com>
2020-05-12 19:46:30 +0800

    update battery

--- src/functions/getDataFromEsp.js
+++ src/functions/getDataFromEsp.js
@@ -1,16 +1,15 @@
 var espdata = "default";
-var battery = "100";
+var storeBattery = "100";
 
 exports.handler = function(event, context, callback) {
   try {
     const jsonData = JSON.parse(event.body);
-    const data = jsonData.data;
-    const battery = jsonData.battery;
-    espdata = data;
+    espdata = jsonData.data;
+    storeBattery = jsonData.battery;
   } catch (e) {}
   //const data = "de";
   return callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ data: data, battery: battery }),
+    body: JSON.stringify({ data: espdata, battery: storeBattery }),
   });
 };
