2dc3e43f0c9f704c967931de5104b17d086af8cf
Huaben Chen <huabench@gmail.com>
2020-05-12 09:19:42 +0800

    update getDataEsp

--- src/functions/getDataFromEsp.js
+++ src/functions/getDataFromEsp.js
@@ -1,6 +1,7 @@
 exports.handler = function(event, context, callback) {
+  const { data } = JSON.parse(event.body);
   callback(null, {
     statusCode: 200,
-    body: "Hello, World",
+    body: "Hello, World the data is " + data,
   });
 };
